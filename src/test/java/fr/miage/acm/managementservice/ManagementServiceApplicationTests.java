be9126f4212969fea34135173f25fd209d8d72cc
Yohan Lussiez <yohan.lussiez@valiantys.com>
2024-03-15 22:10:13 +0100

    First commit

---
+++ src/test/java/fr/miage/acm/managementservice/ManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.acm.managementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
